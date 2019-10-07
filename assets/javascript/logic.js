30ddf4e03e5069c11931d4f9ae3ea4595ef933ee
Francisco Silva <ischawk@gmail.com>
2019-10-07 16:59:33 -0500

    Working with GIF and OMDB apis

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,4 @@
-var gif_themes = ["Up", "Toy Story", "Inside Out", "Lion King","Aladdin","Dumbo"];
+var gif_themes = ["Incredibles", "Toy Story", "Inside Out", "Lion King","Aladdin","Dumbo"];
 var lastSearch = " ";
 var limit = 10;
 
@@ -41,41 +41,8 @@ $("#add-element").on("click", function(event) {
 });
 
 $(document).on("click", ".gifInfo", function(event){
-    
-    $(".gifSection").empty();
-    if (lastSearch === $(this).attr("data-name")){
-        limit = limit+10;
-    }
-    else{
-        limit = 10;
-        lastSearch = $(this).attr("data-name");
-    }
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit="+limit+"&q="+$(this).attr("data-name");
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        console.log(queryURL);
-        var data=response.data;
-        console.log(response);
-        for(var i=0;i<data.length;i++){
-            var newGif = $("<div>");
-            var newCard = $("<div>");
-            var newCardBody = $("<div>");
-            newGif.addClass("col-lg-4 mb-4");
-            newCard.addClass("card");
-            //newCard.html("<img src=\""+data[i].images.original.url+"\" class=\"card-img-top\">");
-            newCard.html("<video id=\"video"+i+"\" class=\"card-img-top\" loop><source src=\""+data[i].images.original.mp4+"\" type=\"video/mp4\"></video>");
-            newCardBody.addClass("card-body");
-            newCardBody.html("<h5 class=\"card-title\">"+data[i].title+"</h5>"+
-                             "<p class=\"card-text\">Rating: "+data[i].rating+"</p>"+
-                             "<a href=\"#\" class=\"btn btn-outline-danger btn-sm\"><i class=\"far fa-heart\"></i></a>");
-            newCard.append(newCardBody);
-            newGif.append(newCard);
-            $(".gifSection").append(newGif);
-        }
-    });
-
+    displayGifs($(this).attr("data-name"));
+    displayMovieInfo($(this).attr("data-name"));
 });
 
 $(document).on("click", ".card-img-top", function(event){
@@ -87,5 +54,145 @@ $(document).on("click", ".card-img-top", function(event){
         $("#"+video).get(0).pause();
     }
 });
+
+function displayGifs(buttonInfo){
+    $(".gifSection").empty();
+    if (lastSearch === buttonInfo) {
+        limit = limit + 10;
+    }
+    else {
+        limit = 10;
+        lastSearch = buttonInfo;
+    }
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=" + limit + "&q=" + buttonInfo;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var data = response.data;
+        for (var i = 0; i < data.length; i++) {
+            var newGif = $("<div>");
+            var newCard = $("<div>");
+            var newCardBody = $("<div>");
+            newGif.addClass("col-lg-4 mb-4");
+            newCard.addClass("card");
+            //newCard.html("<img src=\""+data[i].images.original.url+"\" class=\"card-img-top\">");
+            newCard.html("<video id=\"video" + i + "\" class=\"card-img-top\" loop><source src=\"" + data[i].images.original.mp4 + "\" type=\"video/mp4\"></video>");
+            newCardBody.addClass("card-body");
+            newCardBody.html("<h5 class=\"card-title\">" + data[i].title + "</h5>" +
+                             "<p class=\"card-text\">Rating: " + data[i].rating + "</p>" +
+                             "<a href=\"#\" class=\"btn btn-outline-danger btn-sm\"><i class=\"far fa-heart\"></i></a>");
+            newCard.append(newCardBody);
+            newGif.append(newCard);
+            $(".gifSection").append(newGif);
+        }
+    });
+
+}
+
+function displayMovieInfo(buttonInfo) {
+
+    $(".movieSection").empty();
+    var pages = limit / 10;
+    var imageDisplay;
+    for (var i = 0; i < pages; i++) {
+        var queryURL = "https://www.omdbapi.com/?s=" + buttonInfo + "&type=movie&apikey=trilogy&page=" + (i + 1);
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            for (var j = 0; j < response.Search.length; j++) {
+                queryURL = "http://www.omdbapi.com/?i=" + response.Search[j].imdbID + "&apikey=trilogy";
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response2) {
+                    var newMovie = $("<div>");
+                    var newCard = $("<div>");
+                    var newRow = $("<div>");
+                    var newImage = $("<div>");
+                    var newCol = $("<div>");
+                    var newCardBlock = $("<div>");
+                    newMovie.addClass("col-lg-6 mb-6");
+                    newCard.addClass("card");
+                    newRow.addClass("row no-gutters");
+                    newImage.addClass("col-auto");
+                    if (response2.Poster.length>5){
+                        imageDisplay = "\""+response2.Poster+"\"";
+                    }
+                    else{
+                        imageDisplay = "\"assets/images/error.jpg\" width=300 height=300 ";
+                    }
+                    newImage.html("<img src="+imageDisplay+" class=\"img-fluid\" alt=\"\">");
+                    newCol.addClass("col");
+                    newCardBlock.addClass("card-block px-2");
+                    var ratings = response2.Ratings;
+                    var ratingElement;
+                    for(var i=0;i<ratings.length;i++){
+                        if (i === 0){
+                            ratingElement = ratings[i].Source+": "+ratings[i].Value+"<br>";
+                        }
+                        else{
+                           ratingElement = ratings[i].Source+": "+ratings[i].Value+"<br>"+ratingElement;
+                        }
+                        console.log(ratingElement);
+                    }
+                    newCardBlock.html("<h4 class=\"card-title\">"+response2.Title+"</h4>"+
+                                      "<p class=\"card-text\">"+response2.Plot+"</p>"+
+                                      "<p class=\"card-text\">Released: "+response2.Released+"</p>"+
+                                      "<p class=\"card-text\">Ratings: </p>"+
+                                      ratingElement
+                                      );
+                    newCol.append(newCardBlock);
+                    newRow.append(newImage);
+                    newRow.append(newCol);
+                    newCard.append(newRow);
+                    newMovie.append(newCard);
+                    $(".movieSection").append(newMovie);
+                })
+            }
+/*                <div class="col-lg-6 mb-6">
+                    <div class="card">
+                        <div class="row no-gutters">
+                            <div class="col-auto">
+                                <img src="https://m.media-amazon.com/images/M/MV5BOTc2OTA1MDM4M15BMl5BanBnXkFtZTgwNjczMDk5MjE@._V1_SX300.jpg"
+                                    class="img-fluid" alt="">
+                            </div>
+                            <div class="col">
+                                <div class="card-block px-2">
+                                    <h4 class="card-title">Title</h4>
+                                    <p class="card-text">Description Description Description Description Description Description
+                                        Description
+                                        Description Description Description Description Description Description Description
+                                        Description
+                                        Description Description Description</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            /*<div class="col-lg-6 mb-6">
+            <div class="card flex-row flex-wrap">
+                <div class="card-header border-0">
+                    <img src="https://m.media-amazon.com/images/M/MV5BOTc2OTA1MDM4M15BMl5BanBnXkFtZTgwNjczMDk5MjE@._V1_SX300.jpg"
+                        alt="">
+                </div>
+                <div class="card-block px-2">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Description</p>
+                    <a href="#" class="btn btn-primary">BUTTON</a>
+                </div>
+                <div class="w-100"></div>
+                <div class="card-footer w-100 text-muted">
+                    FOOTER
+                </div>
+            </div>
+        </div>*/
+
+        })
+    }
+}
 
 renderButtons();
